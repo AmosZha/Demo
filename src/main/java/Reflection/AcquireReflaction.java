5531b1faace48f4bb363d8f4b03b72e141cf9ab7
“me <zhaoyi19925@outlook.com>
2023-11-19 16:49:26 +0800

    通过反射获取-通用参数类型（getGenericParameterTypes）

--- src/main/java/Reflection/AcquireReflaction.java
+++ src/main/java/Reflection/AcquireReflaction.java
@@ -1,10 +1,13 @@
 package Reflection;
 
+import DesignPattern.SingletonTest;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.io.Serializable;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 /**
  *  获取反射的三种方法
@@ -14,52 +17,78 @@ public class AcquireReflaction {
 
     public static void main(String[] args) throws ClassNotFoundException {
 
-        //一、通过反射获取类的CLass对象
-        Class<?> c1 = Class.forName("Reflection.User");
-        System.out.println(c1);
+//        //一、通过反射获取类的CLass对象
+//        Class<?> c1 = Class.forName("Reflection.User");
+//        System.out.println(c1);
+//
+//        //获取c1构造器
+//        try {
+//
+//            Constructor< ? > constructor = c1.getConstructor(String.class, int.class);
+//            System.out.println("获取c1全部参数构造器：" + constructor);
+//
+//        } catch (NoSuchMethodException e) {
+//            e.printStackTrace();
+//        }
+//
+//        // 一个类在内存中只有一个class对象
+//        // 一个类敲加载后 的整个结构都会被封装在CLass 对象中
+//        Class<?> c2 = Class.forName("Reflection.User");
+//        Class<?> c3 = Class.forName("Reflection.User");
+//        Class<?> c4 = Class.forName("Reflection.User");
+//
+//        System.out.println("c2:" + c2.hashCode());
+//        System.out.println("c3:" + c3.hashCode());
+//        System.out.println("c4:" + c4.hashCode());
+//
+//
+//        //二、Object.getClass();
+//        // .getClass()方法获取的Class和反射获取类的CLass对象
+//        User user = new User("lucy", 11);
+//        Class<? extends User> c5 = user.getClass();
+//        System.out.println(c5);
+//        System.out.println("c5:" + c5.hashCode());
+//
+//        //三、User.class
+//        Class<User> c6 = User.class;
+//        System.out.println("c6:" + c6.hashCode());
+//
+//        //四、基本内置类型的包装类都有一个TYPE属性
+//        Class<Integer> ingegerClass = Integer.TYPE;
+//        System.out.println("c7:" +ingegerClass);
+//        System.out.println("c8:" +ingegerClass.hashCode());
+//
+//        //只要元素类型与纬度一样，就是同一个Class
+//        Integer[] integer10 = new Integer[10];
+//        Integer[] integer100 = new Integer[100];
+//        System.out.println("c9:" +integer10.getClass().hashCode());
+//        System.out.println("c10:" +integer100.getClass().hashCode());
 
-        //获取c1构造器
+
+
+        SingletonTest singletonTest = new SingletonTest();
+        singletonTest.getClass();
+
+        Class<SingletonTest> singletonTestClass = SingletonTest.class;
+
+
         try {
-
-            Constructor< ? > constructor = c1.getConstructor(String.class, int.class);
-            System.out.println("获取c1全部参数构造器：" + constructor);
-
-        } catch (NoSuchMethodException e) {
+            Class<?> aClass = Class.forName("DesignPattern.SingletonTest");
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
 
-        // 一个类在内存中只有一个class对象
-        // 一个类敲加载后 的整个结构都会被封装在CLass 对象中
-        Class<?> c2 = Class.forName("Reflection.User");
-        Class<?> c3 = Class.forName("Reflection.User");
-        Class<?> c4 = Class.forName("Reflection.User");
+        try {
+            SingletonTest singletonTest1 = singletonTestClass.newInstance();
 
-        System.out.println("c2:" + c2.hashCode());
-        System.out.println("c3:" + c3.hashCode());
-        System.out.println("c4:" + c4.hashCode());
+            Arrays.stream(singletonTestClass.getMethods()).forEach(System.out::println);
 
 
-        //二、Object.getClass();
-        // .getClass()方法获取的Class和反射获取类的CLass对象
-        User user = new User("lucy", 11);
-        Class<? extends User> c5 = user.getClass();
-        System.out.println(c5);
-        System.out.println("c5:" + c5.hashCode());
-
-        //三、User.class
-        Class<User> c6 = User.class;
-        System.out.println("c6:" + c6.hashCode());
-
-        //四、基本内置类型的包装类都有一个TYPE属性
-        Class<Integer> ingegerClass = Integer.TYPE;
-        System.out.println("c7:" +ingegerClass);
-        System.out.println("c8:" +ingegerClass.hashCode());
-
-        //只要元素类型与纬度一样，就是同一个Class
-        Integer[] integer10 = new Integer[10];
-        Integer[] integer100 = new Integer[100];
-        System.out.println("c9:" +integer10.getClass().hashCode());
-        System.out.println("c10:" +integer100.getClass().hashCode());
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
 
     }
 }
---
+++ src/main/java/Reflection/Generics.java
@@ -0,0 +1,65 @@
+package Reflection;
+
+import com.sun.xml.internal.org.jvnet.fastinfoset.stax.LowLevelFastInfosetStreamWriter;
+import javafx.scene.effect.Reflection;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * （注：系统名称 - 模块名称 - 功能名称）
+ * Copyright 1998-2023 company dept
+ * @author zhaoYI 2023-11-19 15:45
+ * @version 0.1
+ * @date 2023-11-19（注：最后更新日期）
+ * Modification History:
+ *    Date         Author       Version     Description
+ * ****************************************************
+ *  2023-11-19   zhaoYI       0.1         初始开发
+ **/
+
+public class Generics {
+
+    public void showName(Map<String, User> map, List<User> list){
+        System.out.println("SHOW NAME");
+    }
+
+    public void showName(int age){
+        System.out.println(age);
+    }
+
+    /**
+     * 通过反射获取具体参数类型
+     */
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException {
+
+        //获取映射类型
+        //Class<?> genericsClass = Class.forName("Reflection.Generics");
+        //Class<Generics> genericsClass = Generics.class;
+        Generics generics = new Generics();
+        Class<? extends Generics> genericsClass = generics.getClass();
+
+        //通过反射获取指定方法
+        Method showNameMethods = genericsClass.getMethod("showName", Map.class, List.class);
+        //获取-通用参数-集合
+        Type[] genericParameterTypes = showNameMethods.getGenericParameterTypes();
+
+        for (Type genericParameterType: genericParameterTypes){
+            //通用集合中的参数类别
+            System.out.println("genericParameterTypes: "+ genericParameterType);
+
+            if(genericParameterType instanceof ParameterizedType) {
+                //获取-实际参数类别集合
+                Type[] actualTypeArguments = ((ParameterizedType) genericParameterType).getActualTypeArguments();
+
+                for (Type actualTypeArgument : actualTypeArguments){
+                    System.out.println(actualTypeArgument);
+                }
+            }
+        }
+
+    }
+}
