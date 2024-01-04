550fadcb4b716d94ae9ac397d6c974afe621f859
“me <zhaoyi19925@outlook.com>
2024-01-05 00:36:04 +0800

    modify:优化

--- src/main/java/com/zhao/DesignPattern/ObserverPattern/Client.java
+++ src/main/java/com/zhao/DesignPattern/ObserverPattern/Client.java
@@ -1,5 +1,15 @@
 package com.zhao.DesignPattern.ObserverPattern;
 
+/**
+ *  ==================Java观察者模式【行为设计型】======================
+ *  Observer Pattern
+ *  它定义了对象之间的一对多依赖关系，当一个对象的状态发生改变时，
+ *  所有依赖于它的对象都会得到通知并被自动更新。
+ *  =============================================================
+ * Description: 客户
+ * Author: <a href="">zhaoYi</a>
+ * Date: 2024/01/05
+ */
 public class Client {
 
     public static void main(String[] args) {
